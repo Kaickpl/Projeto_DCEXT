a0b95aa879d17b7d33188e52d12f11504f1b8e1c
Marcus <mvc7730@gmail.com>
2025-10-23 02:59:55 -0300

    feat: adição repositórios de atividade e endereco

--- src/main/java/Repository/Paciente.java
+++ src/main/java/Repository/Paciente.java
@@ -1,7 +1,9 @@
 package Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface Paciente extends JpaRepository<Paciente, Integer> {
     public Paciente findByNome(String nome);
     public Paciente findByResponsavel(String responsavel);
